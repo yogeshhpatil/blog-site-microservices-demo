4b735d623a3e857c59b2adbb8c1814145a81ca01
Yogesh Patil <yogesh.hpatil4@gmail.com>
2019-08-21 18:48:43 +0530

    added article-library service

---
+++ service-article-library/src/main/java/com/blog/application/servicearticlelibrary/ServiceArticleLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.blog.application.servicearticlelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceArticleLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceArticleLibraryApplication.class, args);
+	}
+
+}
