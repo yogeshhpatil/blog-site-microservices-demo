b7eee115c6467ca64715fd00510db36c9365a95e
Yogesh Patil <yogesh.hpatil4@gmail.com>
2019-08-23 19:44:26 +0530

    refactored Library service

---
+++ service-article-library/src/main/java/com/blog/application/servicearticlelibrary/service/ArticlesService.java
@@ -0,0 +1,28 @@
+package com.blog.application.servicearticlelibrary.service;
+
+import com.blog.application.servicearticlelibrary.model.ArticlesList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class ArticlesService {
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    @Value("${name.articles-service}")
+    private String serviceName;
+
+    public ArticlesList getAllArticles() {
+        return webClientBuilder.build()
+                .method(HttpMethod.GET)
+                .uri("http://" + serviceName + "/v1/articles")
+                .retrieve()
+                .bodyToMono(ArticlesList.class)
+                .block();
+    }
+
+}
--- service-article-library/src/main/java/com/blog/application/servicearticlelibrary/service/LibraryService.java
+++ service-article-library/src/main/java/com/blog/application/servicearticlelibrary/service/LibraryService.java
@@ -1,48 +1,24 @@
 package com.blog.application.servicearticlelibrary.service;
 
-import com.blog.application.servicearticlelibrary.model.Article;
 import com.blog.application.servicearticlelibrary.model.ArticlesList;
-import com.blog.application.servicearticlelibrary.model.CommentList;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class LibraryService {
 
     @Autowired
-    WebClient.Builder webClientBuilder;
+    private ArticlesService articlesService;
+
+    @Autowired
+    private CommentsService commentsService;
 
     public ArticlesList getAllArticles() {
-        // getting articles from service-articles
-        ArticlesList articlesList = webClientBuilder.build()
-                .method(HttpMethod.GET)
-                .uri("http://service-articles/v1/articles")
-                .retrieve()
-                .bodyToMono(ArticlesList.class)
-                .block();
 
-        // getting comments for each article from service-comments
-        List<Article> articles = articlesList.getArticles()
-                .stream()
-                .map(article -> {
-                    CommentList commentList = webClientBuilder.build()
-                            .method(HttpMethod.GET)
-                            .uri("http://service-comments/v1/comments?postId=" + article.getId())
-                            .retrieve()
-                            .bodyToMono(CommentList.class)
-                            .block();
+        ArticlesList allArticles = articlesService.getAllArticles();
 
-                    article.setCommentList(commentList);
-                    return article;
-                }).collect(Collectors.toList());
+        ArticlesList commentsForArticles = commentsService.getCommentsForArticles(allArticles);
 
-        articlesList.setArticles(articles);
-
-        return articlesList;
+        return commentsForArticles;
     }
 }
